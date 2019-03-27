9961cbf8a61a76f0ce9efdb396c6be6622272504
SamuelAripov <aripov85cm@gmail.com>
2019-03-27 19:44:46 -0400

    first commit

---
+++ public/JS/app.js
@@ -0,0 +1,33 @@
+let units = 'auto'
+let tempType = 'F'
+
+let tempSlider = document.getElementById('tempUnit')
+
+let fetchTheWeather = () => {
+    let ipAddress = new Promise((reject, resolve) => {
+        fetch('http://ip-api.com/json').then((res) => {
+            return res.json().then((data) => {
+                resolve(data)
+            })
+        })
+    })
+    ipAddress.then((value) => {
+    }).catch(value => {
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a7056b73015ab391704090db56a47c69/${value.lat},${value.lon}?units=${units}`).then((res) => {
+            res.json().then((data) => {
+                document.querySelector('#weatherItself').innerHTML = `Weather in ${value.city} ${value.regionName} ${value.country} is:<br>${data.daily.summary}<br>${data.daily.data[0].summary}<br>And the current temperature is about ${data.currently.temperature}°${tempType}<br>There is ${data.currently.precipProbability}% chance of rain`
+            })
+        })
+    })
+}
+fetchTheWeather()
+tempSlider.addEventListener("click", e => {
+    if (tempSlider.checked == true) {
+        units = 'si'
+        tempType = 'C'
+    } else {
+        units = 'auto'
+        tempType = 'F'
+    }
+    fetchTheWeather()
+})
---
+++ src/app.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+//111111111111111111111111111111111111111111111111
+//Defining values for request
+let units = 'auto'
+let tempType = 'F'
+let lang = 'en'
+//Defining paths
+const publicDirPath = express.static(path.join(__dirname, '../public'))
+const viewsDirPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setting up view engine and handlebars
+app.set('view engine', 'hbs')
+app.set('views', viewsDirPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static dir to serve
+app.use(publicDirPath)
+
+app.get('', (req, res) => {
+    if (req.query.units == 'C' || req.query.units == 'c') {
+        tempType = 'C'
+        units = 'si'
+    }
+    if (req.query.units) {
+        lang = req.query.lang
+    }
+    res.render('index', {
+        title: 'Weather',
+        text: `The weather (${tempType}°) in your area is: `,
+    })
+})
+app.get('/about', (req, res) => {
+    res.render(`about`, {
+        title: 'About me!',
+        text: 'A young developer with big goals.',
+        href: '/'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        text: 'Having trouble? Look through the help articles to find a solution!',
+        href: '/'
+    })
+})
+app.get('/help/*', (req, res) => {
+    res.render('help', {
+        title: 'Whoops! This help article was not found! Sorry about that',
+        href: '/help'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('help', {
+        title: '404 Page not found.',
+        href: '/'
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
