dab2bf677362455f17ce6dd5be1cc8691b75e5d5
SamuelAripov <aripov85cm@gmail.com>
2019-03-29 18:05:22 -0400

    Fixed the weather info style

--- public/JS/app.js
+++ public/JS/app.js
@@ -15,7 +15,7 @@ let fetchTheWeather = () => {
     }).catch(value => {
         fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a7056b73015ab391704090db56a47c69/${value.latitude},${value.longitude}?units=${units}`).then((res) => {
             res.json().then((data) => {
-                document.querySelector('#weatherItself').innerHTML = `Weather in ${value.city} ${value.region} ${value.country_name} is:<br>${data.daily.summary}<br>${data.daily.data[0].summary}<br>And the current temperature is about ${data.currently.temperature}°${tempType}<br>There is ${data.currently.precipProbability}% chance of rain`
+                document.querySelector('#weatherItself').innerHTML = `Weather in ${value.city} ${value.region} ${value.country_name} is:<br><br>${data.daily.summary}<br>${data.daily.data[0].summary}<br>And the current temperature is about ${data.currently.temperature}°${tempType}<br>There is ${data.currently.precipProbability}% chance of rain`
             })
         })
     })
